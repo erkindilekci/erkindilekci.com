dfb593e609a0cdc97336bd0f6e29d05687d659c4
Erkin Dilekci <dilekcierkin@gmail.com>
2024-02-23 19:41:18 +0300

    Update ProjectContainer.jsx

--- src/components/main/projects/ProjectContainer.jsx
+++ src/components/main/projects/ProjectContainer.jsx
@@ -1,14 +1,65 @@
-import {useState} from "react";
+// import {useState} from "react";
+//
+// function ProjectContainer({project}) {
+//     const [isHovering, setIsHovering] = useState(false);
+//
+//     return (
+//         <div
+//             className="flex w-auto flex-col justify-between rounded-3xl p-6 shadow-lg transition-all max-w-72 bg-containerColorLightTheme dark:bg-containerColorDarkTheme"
+//             onMouseEnter={() => setIsHovering(true)}
+//             onMouseLeave={() => setIsHovering(false)}
+//             onClick={() => setIsHovering(isClicked => !isClicked)}
+//         >
+//             <div>
+//                 {project.image && <img className="rounded-md transition-all" src={project.image} alt={project.title}/>}
+//                 <h3 className="my-2 text-base font-medium transition-all text-titleColorLightTheme dark:text-gray-100 md:my-3 md:text-lg">{project.title}</h3>
+//                 <ul>{project.technologies.map((t, i) => <li
+//                     className="mb-auto text-xs transition-all py-[1px] text-textColorLightTheme dark:text-textColorDarkTheme md:text-sm"
+//                     key={i}>{t}</li>)}
+//                 </ul>
+//             </div>
+//
+//             <div className={`${!isHovering ? 'hidden' : 'flex'} w-full transition-all mt-4 md:-mt-4 items-baseline ${project.links.length === 2 ? 'justify-between' : 'justify-center'}`}>
+//                 {project.links.map((link, index) => (
+//                     <a href={link} target="_blank">
+//                         {index % 2 === 0
+//                             ? <button
+//                                 className="text-xs rounded-lg border-2 border-solid bg-transparent px-4 py-2 transition-all border-firstColor text-titleColorLightTheme hover:bg-firstColorAlt dark:text-titleColorDarkTheme md:mt-10">GitHub</button>
+//                             : <button
+//                                 className="text-xs rounded-lg px-4 transition-all bg-firstColor py-[0.55rem] text-titleColorLightTheme hover:bg-firstColorAlt dark:text-titleColorDarkTheme md:mt-6">Live
+//                                 Demo</button>
+//                         }
+//                     </a>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// }
+//
+// export default ProjectContainer;
+
+
+import {useEffect, useState} from "react";
 
 function ProjectContainer({project}) {
     const [isHovering, setIsHovering] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+
+    const updateScreenSize = () => {
+        setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", updateScreenSize);
+        return () => window.removeEventListener("resize", updateScreenSize);
+    }, []);
 
     return (
         <div
             className="flex w-auto flex-col justify-between rounded-3xl p-6 shadow-lg transition-all max-w-72 bg-containerColorLightTheme dark:bg-containerColorDarkTheme"
-            onMouseEnter={() => setIsHovering(true)}
-            onMouseLeave={() => setIsHovering(false)}
-            onClick={() => setIsHovering(isClicked => !isClicked)}
+            onMouseEnter={() => !isSmallScreen && setIsHovering(true)}
+            onMouseLeave={() => !isSmallScreen && setIsHovering(false)}
+            onClick={() => isSmallScreen && setIsHovering(isClicked => !isClicked)}
         >
             <div>
                 {project.image && <img className="rounded-md transition-all" src={project.image} alt={project.title}/>}
@@ -19,14 +70,15 @@ function ProjectContainer({project}) {
                 </ul>
             </div>
 
-            <div className={`${!isHovering ? 'hidden' : 'flex'} w-full transition-all -mt-4 items-baseline ${project.links.length === 2 ? 'justify-between' : 'justify-center'}`}>
+            <div
+                className={`${!isHovering ? 'hidden' : 'flex'} w-full transition-all mt-4 md:-mt-4 items-baseline ${project.links.length === 2 ? 'justify-between' : 'justify-center'}`}>
                 {project.links.map((link, index) => (
                     <a href={link} target="_blank">
                         {index % 2 === 0
                             ? <button
-                                className="text-xs rounded-lg border-2 border-solid bg-transparent px-4 py-2 transition-all border-firstColor text-titleColorLightTheme hover:bg-firstColorAlt dark:text-titleColorDarkTheme md:mt-10">GitHub</button>
+                                className="text-xs md:text-sm rounded-lg border-2 border-solid bg-transparent px-4 py-2 transition-all border-firstColor text-titleColorLightTheme hover:bg-firstColorAlt dark:text-titleColorDarkTheme md:mt-10">GitHub</button>
                             : <button
-                                className="text-xs rounded-lg px-4 transition-all bg-firstColor py-[0.55rem] text-titleColorLightTheme hover:bg-firstColorAlt dark:text-titleColorDarkTheme md:mt-6">Live
+                                className="text-xs md:text-sm rounded-lg px-4 transition-all bg-firstColor py-[0.55rem] text-titleColorLightTheme hover:bg-firstColorAlt dark:text-titleColorDarkTheme md:mt-6">Live
                                 Demo</button>
                         }
                     </a>
@@ -37,4 +89,3 @@ function ProjectContainer({project}) {
 }
 
 export default ProjectContainer;
-
