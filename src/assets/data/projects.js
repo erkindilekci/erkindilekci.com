3c84d92aa31966b36b79627be4c2ef98bbce13f0
Erkin Dilekçi <109282517+erkindilekci@users.noreply.github.com>
2024-09-04 08:33:15 +0300

    Update projects.js

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -15,7 +15,7 @@ const projects = [
             'Tailwind CSS',
             'js-cookie'
         ],
-        links: ['https://erkindilekci-ims.netlify.app/', 'https://github.com/erkindilekci/inventory-management-system']
+        links: ['https://github.com/erkindilekci/inventory-management-system', 'https://erkindilekci-ims.netlify.app/']
     },
     {
         title: 'Visi Vault',
